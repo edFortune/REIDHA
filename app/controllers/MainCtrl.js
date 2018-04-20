45d816011a6a00298d2d7e0acae2cb50a2bec2a8
edFortune <fortune.edmael1@gmail.com>
2018-04-20 13:04:12 -0400

    add firebase framework

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -1,5 +1,41 @@
-mainApp.controller(function MainCtrl($scope) {
+mainApp.controller('MainCtrl', function MainCtrl($scope) {
+
+    //
+    $('.ui.dropdown').dropdown();
+    $('.menu .item').tab();
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBAke6SOKWX52KcQtDYDSTG2LD8s39x8EI",
+        authDomain: "reidha-12034.firebaseapp.com",
+        databaseURL: "https://reidha-12034.firebaseio.com",
+        projectId: "reidha-12034",
+        storageBucket: "",
+        messagingSenderId: "528557444361"
+    };
+
+    firebase.initializeApp(config);
+
+    var firestore = firebase.firestore();
+    const $idInput = $("#id");
+
+    const docRef = firestore.doc("recenseurs/" + $idInput.val());
+    const $nomInput = $("#nom");
+    const $prenomInput = $("#prenom");
+
+
+    //
     $scope.myClick = function () {
-        alert("bdhsbdjf");
+
+        docRef.set({
+            Nom: $nomInput.val(),
+            Prenom: $prenomInput.val(),
+            Departement: ""
+        }).then(function () {
+            console.log("Status saved")
+        }).catch(function (error) {
+            console.log("Got an error: ", error)
+        });
+
     }
 });
